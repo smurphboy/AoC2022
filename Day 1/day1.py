9d359ac317bd24df18f0af6f0dd9d90fada3278f
smurphboy <simon.murphy@gmail.com>
2022-12-01 09:11:14 +0000

    Day 1 complete

---
+++ Day 1/day1.py
@@ -0,0 +1,21 @@
+
+with open('Day 1/input.txt') as f:
+    lines = [line for line in f]
+
+elvesfood = []
+totalfood = 0
+for line in lines:
+    if line == '\n':
+        elvesfood.append(totalfood)
+        totalfood = 0
+    else:
+        totalfood = totalfood + int(line)
+
+print ('Part one = '+ str(max(elvesfood)))
+
+theset = frozenset(elvesfood)
+sortedset = sorted(theset, reverse=True)
+
+firstthree = sortedset[0] + sortedset[1] + sortedset[2]
+
+print ('Part Two = ' + str(firstthree))
