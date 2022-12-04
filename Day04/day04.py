f7c441e160bd222b2ad667c7525fd392d2eb575c
Simon Murphy <simon.murphy@gmail.com>
2022-12-04 12:20:07 +0000

    Part One complete

--- Day04/day04.py
+++ Day04/day04.py
@@ -1,4 +1,4 @@
-with open('Day04/test.txt') as f:
+with open('Day04/input.txt') as f:
     lines = [line for line in f]
 
 totalcontained = 0
@@ -7,18 +7,15 @@ for line in lines:
     assignments = line.split(',')
     first = assignments[0].split('-')
     second = assignments[1].rstrip().split('-')
-    print(first, second)
-    range1 = range(int(first[0]), int(first[1]))
-    range2 = range(int(second[0]), int(second[1]))
+    range1 = range(int(first[0]), int(first[1])+1)
+    range2 = range(int(second[0]), int(second[1])+1)
     x1 = set(range1)
-    if x1.intersection(range2) == x1:
-        totalcontained = totalcontained + 1
-        print ("1st case")
-        continue
     x2 = set(range2)
-    if x2.intersection(range1) == x2:
+    if x1.issubset(range2):
         totalcontained = totalcontained + 1
-        print ('2nd case')
+        continue
+    if x2.issubset(range1):
+        totalcontained = totalcontained + 1
 
 print("Part One: " + str(totalcontained))
 
